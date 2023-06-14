a71cc6e2d52d240407046055fae839b4d23d198d
Hoàng Sang <caubekho00@gmail.com>
2023-06-14 11:09:17 +0700

    Ngoc Quynh

--- js/main.js
+++ js/main.js
@@ -1,11 +1,54 @@
-
+//form đăng nhập 
 document.querySelector("a.login").addEventListener("click", function(event) {
   event.preventDefault();
-  document.getElementById("main-login").style.display = "block";
+  var mainLogin = document.getElementById("main-login");
+  mainLogin.style.display = "block";
+  if (mainLogin.style.display === "block") {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    document.body.style.overflow = 'hidden';
+  }
 });
 
-
-document.getElementById("close").addEventListener("click", function() {
+document.querySelector("#close").addEventListener("click", function(event) {
+  event.preventDefault();
   document.getElementById("main-login").style.display = "none";
+  document.body.style.overflow = 'auto';
 });
 
+//Đăng ký
+document.querySelector("a.login-1").addEventListener("click", function(event) {
+  event.preventDefault();
+  var mainLogin = document.getElementById("main-login");
+  mainLogin.style.display = "none";
+  var mainLogin = document.getElementById("main-login-2");
+  mainLogin.style.display = "block";
+  if (mainLogin.style.display === "block") {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    document.body.style.overflow = 'hidden';
+  }
+});
+
+document.querySelector("#close-2").addEventListener("click", function(event) {
+  event.preventDefault();
+  document.getElementById("main-login-2").style.display = "none";
+  document.body.style.overflow = 'auto';
+});
+
+//trở lại đăng nhập
+document.querySelector("a.login-2").addEventListener("click", function(event) {
+  event.preventDefault();
+  var mainLogin = document.getElementById("main-login-2");
+  mainLogin.style.display = "none";
+  var mainLogin = document.getElementById("main-login");
+  mainLogin.style.display = "block";
+  if (mainLogin.style.display === "block") {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    document.body.style.overflow = 'hidden';
+  }
+});
+
+document.querySelector("#close").addEventListener("click", function(event) {
+  event.preventDefault();
+  document.getElementById("main-login").style.display = "none";
+  document.body.style.overflow = 'auto';
+});
